d1ce9770ed366b6088b49f95848b587f21d7dc47
Denys Filichkin <denys.philichkin@gmail.com>
2023-04-05 17:51:37 +0300

    rewrite

--- main.py
+++ main.py
@@ -1,35 +1,71 @@
-from handler import handler_dict
-from parser_gear import parser_gear_user_input
+import functools
 
-def user_name_input():
-    name_input = input("Hello! What is your name?\nPlease enter: ")
-    print(f"\n{name_input.lower().capitalize()}, nice to meet you. let's start.\n")
+ARRAY = {}
 
-def main():
-    user_name_input()
-    print("""You can use the following commands for your contact list:
-    - add name number - it's simple, to add new contact to your contact list;
-    - change name new-number - to set up new number for contact with this name (if exist);
-    - delete name - to delete the contact with this name from contact list (if exist);
-    - phone name - to see the phone number for this name (if exist);
-    - show all - to see all contacts in your contact list (if you have added at least 1);
-    - good bye / close / exit - to finish work and close session;
-    """)
-    while True:
-        user_input = input("Please enter command: ")
-        result = parser_gear_user_input(user_input=user_input)
-        if len(result) != 2:
-            print(result)
-            continue
-        command, arguments = result
-        command_handler = handler_dict.get(command)
+def input_error(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         try:
-            command_response = command_handler(*arguments)
-            print(command_response)
-        except SystemExit as err:
-            print(str(err))
-            break
+            return func(*args, **kwargs)
+        except IndexError:
+            print("""You have not entered all data!!!
+--------------------------------------------------------------------------------------------------
+for adding new phone number please input:   add name tel.      (example: add Denys 345-45-45)
+for change please input:                    change name tel.   (example: change Denys 2345789)
+for reading please input:                   phone name         (example: phone Denys)
+--------------------------------------------------------------------------------------------------""")
+        except KeyError:
+            print("This user was not found in the phone book!")
+        except ValueError:
+            print("Invalid value. Try again.")
+    return wrapper
 
+
+def welcome_bot(func):
+    def inner(*args, **kwargs):
+        print("-"*32+"\nWelcome to Assistant Console Bot\n"+"-"*32)
+        return func(*args, **kwargs)
+    return inner
+
+
+@input_error
+def handler(enter_input):
+    if enter_input.lower() == "hello":
+        return "May I help you?"
+    
+    if enter_input.lstrip()[0:3].lower() == "add":
+        ARRAY[enter_input.split(" ")[1]] = enter_input.split(" ")[2]
+
+    if "change" in enter_input.lower():
+        ARRAY[enter_input.split(" ")[1]] = enter_input.split(" ")[2]
+
+    if "phone" in enter_input.lower():
+        return ARRAY[enter_input.split(" ")[1]]
+    
+    if "show all" in enter_input.lower():
+        if ARRAY == {}:
+            array_message = "Your contact list is empty."
+        else:
+            array_message = "Display contact list book:\n"
+            for k, v in ARRAY.items():
+                array_message += ("|{:<12}| {:<15}\n".format(k, v))
+        return array_message
+    
+    return "Unknown command, please input correct data or command!"
+
+
+@ welcome_bot
+def main():
+    while True:
+        enter_input = input("Please enter your command: ")
+        if enter_input.strip().lower() in [".", "good bye", "close", "exit", "stop"]:
+            print("Bye! Bye!")
+            break
+        else:
+            print_me = handler(enter_input)
+            if print_me != None:
+                print(print_me)
+            continue
 
 if __name__ == "__main__":
     main()
